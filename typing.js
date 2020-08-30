73b479be267679af8109251d12407bd8e985fe3a
Agrim <agrim19229@iiitd.ac.in>
2020-08-30 13:25:46 +0530

    UI updates

--- typing.js
+++ typing.js
@@ -2,18 +2,19 @@
 let i = 0;
 let txt = 'We Rise By Lifting Others. ';
 let speed = 70;
-      
+let currentText="";
 const typeWriter = () => {
     if (i < txt.length) {   
-        document.getElementById("tag").innerHTML += txt.charAt(i);
+        currentText+= txt.charAt(i);
+        document.getElementById("tag").innerHTML = currentText+"|"
         console.log(document.getElementById("tag"));
         document.getElementById("tag").style.zIndex =1;
         document.getElementById("tag").style.position = "relative";
         i++;
-        if(i==txt.length-3)
-            setTimeout(typeWriter,speed+20);
-        else
-            setTimeout(typeWriter, speed);
+        setTimeout(typeWriter, speed);
+        if(i==txt.length) {
+            document.getElementById("tag").innerHTML = txt;
+        }
 
     }
 }
